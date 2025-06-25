476fe7df732f7d8226dfb42a1662154dbefba2cf
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-06-26 00:27:24 +0300

    Build GET API with serialization and association proxy

--- server/models.py
+++ server/models.py
@@ -1,5 +1,3 @@
-# server/models.py
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
@@ -13,9 +11,10 @@ metadata = MetaData(
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Game(db.Model):
+class Game(db.Model, SerializerMixin):
     __tablename__ = "games"
+
+    serialize_rules = ("-reviews.game",)
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, unique=True)
@@ -27,12 +26,16 @@ class Game(db.Model):
 
     reviews = db.relationship("Review", back_populates="game")
 
+    # Association proxy to get users for this game through reviews
+    users = association_proxy("reviews", "user", creator=lambda user: Review(user=user))
+
     def __repr__(self):
         return f"<Game {self.title} for {self.platform}>"
 
-
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = "reviews"
+
+    serialize_rules = ("-game.reviews", "-user.reviews",)
 
     id = db.Column(db.Integer, primary_key=True)
     score = db.Column(db.Integer)
@@ -49,13 +52,13 @@ class Review(db.Model):
     def __repr__(self):
         return f"<Review ({self.id}) of {self.game}: {self.score}/10>"
 
-
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     __tablename__ = "users"
+
+    serialize_rules = ("-reviews.user",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
